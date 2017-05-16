e5b7f49a8aeac81898fccbd8e157b9942a14f038
dilz <dilip.weblogic@gmail.com>
2017-05-15 20:11:11 -0700

    python first commit

---
+++ firstpython.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+def Hello(name):
+	if name == 'Dilip':
+		print 'Alert: Dilip Mode'
+		name = name + '???'
+	else	
+		print 'Else'
+	name = name + '!!!!!'
+	print 'Hello', name
+	
+def main():
+	Hello(sys.argv[1])
+	
