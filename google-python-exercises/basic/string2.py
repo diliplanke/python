03d0fcea78fcd829e8f4f596494619a9408cc684
Cloud User <ec2-user@ip-172-31-19-44.us-west-1.compute.internal>
2017-05-28 18:33:14 -0400

    adding python exercised

---
+++ google-python-exercises/basic/string2.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Additional basic string exercises
+
+# D. verbing
+# Given a string, if its length is at least 3,
+# add 'ing' to its end.
+# Unless it already ends in 'ing', in which case
+# add 'ly' instead.
+# If the string length is less than 3, leave it unchanged.
+# Return the resulting string.
+def verbing(s):
+  # +++your code here+++
+  if len(s) > 3:
+    if s[-3:] == 'ing':
+      output = s + 'ly'
+    else:
+      output = s + 'ing'
+    return output
+  else:
+    return s
+
+
+# E. not_bad
+# Given a string, find the first appearance of the
+# substring 'not' and 'bad'. If the 'bad' follows
+# the 'not', replace the whole 'not'...'bad' substring
+# with 'good'.
+# Return the resulting string.
+# So 'This dinner is not that bad!' yields:
+# This dinner is good!
+def not_bad(s):
+  # +++your code here+++
+  start = s.find('not')
+  end = s.find('bad')  
+  if start != -1 or end != -1:
+    if start < end:
+      out = s.replace(s[start:end+3],'good') 
+    else:
+      out = s 
+  else:
+    out = s  
+  return out
+
+
+# F. front_back
+# Consider dividing a string into two halves.
+# If the length is even, the front and back halves are the same length.
+# If the length is odd, we'll say that the extra char goes in the front half.
+# e.g. 'abcde', the front half is 'abc', the back half 'de'.
+# Given 2 strings, a and b, return a string of the form
+#  a-front + b-front + a-back + b-back
+def front_back(a, b):
+  # +++your code here+++
+  vari = [a,b]
+  output = []
+  for num in vari:
+    length = len(num)/2
+    if len(num) % 2 == 1:
+      output.append(num[:+(length+1)])
+      #k = [ num[:+(length+1)],num[-length:]]
+    else:
+      # k = [ num[:+length],num[-length:]]  
+      output.append(num[:+length]) 
+    output.append(num[-length:])
+  result = output[0] + output[2] + output[1] + output[3]   
+  return result
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+  if got == expected:
+    prefix = ' OK '
+  else:
+    prefix = '  X '
+  print '%s got: %s expected: %s' % (prefix, repr(got), repr(expected))
+
+
+# main() calls the above functions with interesting inputs,
+# using the above test() to check if the result is correct or not.
+def main():
+  print 'verbing'
+  test(verbing('hail'), 'hailing')
+  test(verbing('swiming'), 'swimingly')
+  test(verbing('do'), 'do')
+
+  print
+  print 'not_bad'
+  test(not_bad('This movie is not so bad'), 'This movie is good')
+  test(not_bad('This dinner is not that bad!'), 'This dinner is good!')
+  test(not_bad('This tea is not hot'), 'This tea is not hot')
+  test(not_bad("It's bad yet not"), "It's bad yet not")
+
+  print
+  print 'front_back'
+  test(front_back('abcd', 'xy'), 'abxcdy')
+  test(front_back('abcde', 'xyz'), 'abcxydez')
+  test(front_back('Kitten', 'Donut'), 'KitDontenut')
+
+if __name__ == '__main__':
+  main()
